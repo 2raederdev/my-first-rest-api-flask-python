4b90e0cd500e370507b5a327bfc9199d3d7a4824
ysidani <daniel.hita@phonr.com>
2020-08-12 08:28:53 +0200

    api finished

--- resources/store.py
+++ resources/store.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource, reqparse
+from flask_jwt_extended import jwt_required
 from models.store import StoreModel
 
 class Store(Resource):
@@ -8,6 +9,7 @@ class Store(Resource):
             return store.json()
         return {'message': 'Store not found'}, 404
 
+    @jwt_required
     def post(self, name):
         if StoreModel.find_by_name(name):
             return {'message': "A store with name '{}' already exists.".format(name)}, 400
@@ -20,6 +22,7 @@ class Store(Resource):
 
         return store.json(), 201
 
+    @jwt_required
     def delete(self, name):
         store = StoreModel.find_by_name(name)
         if store:
@@ -29,4 +32,4 @@ class Store(Resource):
 
 class StoreList(Resource):
     def get(self):
-        return {'stores': [store.json() for store in StoreModel.query.all()]}
+        return {'stores': [store.json() for store in StoreModel.find_all()]}
